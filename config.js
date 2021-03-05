9507e9fa17d10544f86ffc8d23e94f496acd2ccd
Alan Masciangelo <aam32000@gmail.com>
2021-03-04 19:44:44 -0500

    fix url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://alanmasciangelo.github.io/',
   pathPrefix: '/',
   title: 'Alan\'s personal site',
   subtitle: '',
