b53bea20799f4ba82ae8282e51eb3e9bf70527bb
Alan Masciangelo <aam32000@gmail.com>
2021-03-14 10:47:35 -0400

    Post footer tweaks.
    Removed tags - just clutter
    Added author name
    Added css for author photo... seems redundant with comment image
    
    Todo discussions link section

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -1,15 +1,32 @@
 // @flow strict
 import React from 'react';
+import { withPrefix, Link } from 'gatsby';
+import { useSiteMetadata } from '../../../hooks';
 import styles from './Meta.module.scss';
 
 type Props = {
   date: string
 };
 
-const Meta = ({ date }: Props) => (
-  <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</p>
-  </div>
-);
+const Meta = ({ date }: Props) => {
+  const { author } = useSiteMetadata();
+  return ( 
+    <div className={styles['meta']}>
+      <p className={styles['meta__date']}>Published {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })} by {author.name} 
+        {/* <div className={styles['author']}>
+          <Link to="/">
+            <img
+              src={withPrefix(author.photo)}
+              className={styles['author__photo']}
+              width="44"
+              height="44"
+              alt={author.name}
+            />
+          </Link>
+        </div> */}
+      </p>
+    </div>
+  );
+};
 
 export default Meta;
